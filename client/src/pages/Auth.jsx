18f96a26a0b64c064161a6a7d4d75b073b110353
abhishek-poddar9 <abhish.poddar@gmail.com>
2026-04-15 20:19:59 +0530

    Updated content

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -45,7 +45,7 @@ function Auth({isModel = false}) {
                     <BsRobot size={18}/>
 
                 </div>
-                <h2 className='font-semibold text-lg'>InterviewIQ.AI</h2>
+                <h2 className='font-semibold text-lg'>Intervexa AI</h2>
             </div>
 
             <h1 className='text-2xl md:text-3xl font-semibold text-center leading-snug mb-4'>
