0875c6ea5598f48fe74266f9afdca74b56f330b3
abhishek-poddar9 <abhish.poddar@gmail.com>
2026-04-29 03:14:20 +0530

    Fix CORS issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "https://intervexa-ai-backend.onrender.com"
+export const ServerUrl  = "http://localhost:8000"
 
 function App() {
 
